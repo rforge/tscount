3beb1639949be137a1f65c40cd26ca8037078a30
tobiasliboschik <tobiasliboschik@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-02-28 20:08:05 +0000

    Minor corrections in help files
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tscount@16 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/loglin.fit.r
+++ pkg/R/loglin.fit.r
@@ -106,6 +106,7 @@ loglin.fit <- function(ts, model=list(past_obs=NULL, past_mean=NULL, xreg=NULL, 
   }
   paramvec_inter <- as.numeric(inter_optim$par)
   paramvec_final <- as.numeric(final_optim$par)
+  if(p+q+r>0 && all(abs((paramvec_final-paramvec_init)/paramvec_final) < 0.01)) warning("Final estimation is still very close to initial estimation. This might indicate a problem with the optimisation but could also have happended by chance. Please check results carefully.")
   ##############  
   
   ##############
