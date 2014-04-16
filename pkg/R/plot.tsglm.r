f0e6e0c6f2d152eb8f90599b01dda257f8adc3df
tobiasliboschik <tobiasliboschik@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-04-16 15:05:47 +0000

    Changed definition of PIT histogram Modified model assessment graphics
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tscount@19 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/plot.tsglm.r
+++ pkg/R/plot.tsglm.r
@@ -2,9 +2,16 @@ plot.tsglm <- function(x, ask=TRUE, ...){
   op <- par(ask=ask)
   residu <- residuals(x, type="pearson")
   acf(residu, main="ACF of Pearson residuals")
-  hist(residu, main="Histogram of Pearson residuals", xlab="Residuals")
-  plot(1:length(x$ts), residu, xlab="Time", ylab="Residuals", main="Pearson Residuals over time")
-  if(require(MASS)) cpgram(residu, main="Cumulative periodogram of Pearson residuals")
+  #hist(residu, main="Histogram of Pearson residuals", xlab="Residuals")
+  plot(residu, type="o", xlab="Time", ylab="Residuals", main="Pearson residuals over time")
+  if(require(MASS)){
+    par(ann=FALSE)
+    cpgram(residu, main="")
+    title(main="Cumulative periodogram of Pearson residuals", xlab="Frequency")
+    par(ann=TRUE)
+  }
+  pit(x)
+  marcal(x)
   par(op)
   invisible()
 }
--- pkg/R/print.summary.tsglm.r
+++ pkg/R/print.summary.tsglm.r
@@ -2,7 +2,7 @@ print.summary.tsglm <- function(x, ...){
   if(length(coef(x)) > 0){
     cat("\nCall:\n", paste(deparse(x$call), sep="\n", collapse = "\n"), "\n\n", sep="")
     cat("Coefficients:\n")
-    print(format.data.frame(as.data.frame(coef(x)), digits=3), print.gap=2, quote=FALSE)
+    print(format.data.frame(as.data.frame(coef(x)), digits=3), print.gap=2, quote=FALSE, na.print="")
     cat(
       "\nLink function: ", x$link,
       "\nDistribution family: ", x$distr, if(x$distr=="nbinom"){" (with additional dispersion parameter 'size')"}else{NULL},
